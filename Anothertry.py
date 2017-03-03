ae0b4a585cdb2cf6f8b2b8a2e290b9614cf0676a
J <txdyt.j@gmail.com>
2017-03-03 22:35:12 +0800

    some simplest code

---
+++ Anothertry.py
@@ -0,0 +1,5 @@
+# TODO: 标签
+m = 0
+for x in list(range(100)):
+    m = m + x
+print(m)
---
+++ Mycode1.py
@@ -0,0 +1,18 @@
+'''
+print ('Please enter your name:')
+name = input ()
+print ('hello',name)
+print ('I can do add operation for you. Please enter two numbers:')
+num1 = int (input ())
+num2 = int (input ())
+print ('The result is', num1 + num2)
+'''
+#求输入整数的绝对值
+a = int(input())
+if a >=0:
+    print (a)
+else:
+    print (-a)
+print ('''I
+love
+bobo''')
